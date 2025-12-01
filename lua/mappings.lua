e0e26621e354ef299ae38555c8db799de960dbcc
yanke1311 <yanke1311@126.com>
2025-12-01 15:36:29 +0800

    add my own config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,15 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<F5>", "<cmd>CMakeRun<CR>", { desc = "cmake run" })
+map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "tab next" })
+map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "tab next" })
+
+-- meno --
+-- nvim-tree-lua
+-- C-t open new tab
+-- C-v open vertical
+-- C-x open horizontal
+--
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,28 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "easymotion/vim-easymotion",
+    keys = {
+      { "<leader><leader>", "<plug>(easymotion-overwin-f)", desc = "Jump to location", mode = "n"},
+      { "<leader><leader>", "<plug>(easymotion-bd-f)", desc = "Jump to location", mode = "v"},
+    },
+  },
+
+  {
+    "Civitasv/cmake-tools.nvim", opts = {}
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
